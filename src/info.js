77a97d817c0472131e44005cdf9074e789c775d7
ZachJHall <zachjohnhall@gmail.com>
2019-08-09 01:27:05 +0000

    Added default mainphoto and fixed link glitch

--- src/info.js
+++ src/info.js
@@ -1,19 +1,21 @@
 import React from 'react';
 
-export const FullName = '' //Type your name between the quotes
+export const FullName = '' //Copy and paste your name between the quotes
 
-export const GithubLink = '' //Type your Github link within the quotes
+export const GithubLink = '' // your Github link within the quotes
 
-export const LinkedinLink = '' //Type your Linkedin link within the quotes
+export const LinkedinLink = '' //Copy and paste your Linkedin link within the quotes
 
 export const Email = 'mailto:#' //Replace # with email
 
-export const JobTitle = '' //Type your job title within the quotes
+export const JobTitle = '' //Copy and paste your job title within the quotes
 
-export const CompanyName = '' //Type your company/school name within the quotes
+export const CompanyName = '' //Copy and paste your company/school name within the quotes
 
-export const CompanyWebsite = '' //Type your company/school name within the quotes
+export const CompanyWebsite = '' //Copy and paste your company/school name within the quotes
 
-//Name your personal image mainphoto.png and place it in the /src/images folder
+//Replace /src/images/mainphoto.png with your own photo, and name it mainphoto.png
+
+//https:// must come before your links
 
 //Created by Zach Hall, https://github.com/ZachJHall
